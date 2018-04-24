5497bac91d1d8749a98799fef8a9e9e237a682c8
jaslokunal <v-jaslc@microsoft.com>
2018-04-24 12:46:33 +0530

    Add files via upload

---
+++ CircularArrayRotation.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int q = in.nextInt();
+        int[] a = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
+        }
+        int[] rotated = new int[n];
+        if(n!=k && k>n){k=k%n;}
+        for(int a0 = 0; a0 < q; a0++){
+            int m = in.nextInt();
+            for(int i=0; i<n; i++){
+                if((i-k)>=0){rotated[i] = a[i-k];}
+                else{rotated[i]=a[n+i-k];}
+            }
+        System.out.println(rotated[m]);    
+        }
+    }
+}
